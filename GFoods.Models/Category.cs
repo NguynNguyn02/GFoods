bd6d8be7ba8ad7f915b708649b7d1dd499df572b
NguynNguyn02 <trungnguyenna02@gmail.com>
2024-02-05 08:34:06 +0700

    Add project files.

---
+++ GFoods.Models/Category.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GFoods.Models
+{
+    public class Category
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        [DisplayName("Category Name")]
+        public string Name { get; set; }
+        [Required]
+        [Range(1, 100, ErrorMessage = "Display Order must be between 1-100")]
+        public int DisplayOrder { get; set; }
+
+    }
+}
